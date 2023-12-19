69591be2b589d214e9be5127673b3e9753b4cf69
doguerg <36088566+doguerg@users.noreply.github.com>
2023-12-19 10:40:14 +0300

    Update ekle.sh

--- ekle.sh
+++ ekle.sh
@@ -1,18 +1,27 @@
-#!/bin/sh
-new_entries="3.76.147.207 nlb.iot.mainnet.helium.io
+#!/bin/bash
+
+base_path="/var/lib/docker/containers"
+text_to_check="helium"
+text_to_add="3.76.147.207 nlb.iot.mainnet.helium.io
 18.193.232.30 nlb.iot.mainnet.helium.io
 3.122.172.188 nlb.iot.mainnet.helium.io
 3.76.147.207 mainnet-pociot.helium.io
 18.193.232.30 mainnet-pociot.helium.io
 3.122.172.188 mainnet-pociot.helium.io"
-target_folder="/var/lib/docker/containers"
-for container_id in "$target_folder"/*
-do
-    hosts_file="$container_id/hosts"
+
+# Find all subdirectories under the base path
+for folder in "$base_path"/*/; do
+    hosts_file="$folder/hosts"
+
+    # Check if the hosts file exists
     if [ -f "$hosts_file" ]; then
-        echo -e "$new_entries" >> "$hosts_file"
-        echo "IP adresleri basariyla eklendi: $hosts_file"
-    else
-        echo "Hata: Yanlis yerde calistirdin ya da bu makinede $hosts_file klasoru yok."
+        # Check if the file contains the specified text
+        if ! grep -q "$text_to_check" "$hosts_file"; then
+            # If not, append the text to the end of the file
+            echo -e "$text_to_add" >>"$hosts_file"
+            echo "Kayıtlar $hosts_file'na eklendi"
+        else
+            echo "Kayıtlar $hosts_file dosyasında zaten var."
+        fi
     fi
 done
